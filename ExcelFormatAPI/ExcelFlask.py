59e28509ad422041040480d361c7cc99cc0e3cfc
Jared <jarefe2000@gmail.com>
2025-04-30 08:49:25 -0500

    Print lines removed

--- ExcelFormatAPI/ExcelFlask.py
+++ ExcelFormatAPI/ExcelFlask.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 URL = "https://api.smartimageserve.com/upload"
 
 # FIXME: edit to correct filepath for dash server
-# TODO: remove print lines when finalizing
 FILE_DIRECTORY = './static/downloads'
 if not os.path.exists(FILE_DIRECTORY):
     os.makedirs(FILE_DIRECTORY, exist_ok=True)
@@ -21,27 +20,23 @@ def format_testing_report():
         if not json_data:
             return jsonify({'error':'No JSON data provided'})
 
-        # format JSON data
+        # Format JSON data
         processed_workbook = format_JSON_data(json_data)
-        print('JSON data formatted')
 
-        # generate unique file name
+        # Generate unique file name
         file_id = str(uuid.uuid4()) # unique identifier for file
-        print(f'File ID: {file_id}')
         file_name = f'{file_id}.xlsx'
-        print(f'File name: {file_name}')
         file_path = os.path.join(FILE_DIRECTORY, file_name)
-        print(f'File path: {file_path}')
 
-        # save to file under filepath
+        # Save to file under filepath
         try:
             processed_workbook.save(file_path)
             print(f'File saved to: {file_path}')
         except Exception as e:
             print(f'Error saving file: {e}')
-        # generate URL
-        download_url = f'/static/downloads/{file_name}'
-        download_url = request.url_root[:-1] + download_url
+
+        # Generate URL
+        download_url = f'{request.url_root[:-1]}/static/downloads/{file_name}'
 
         payload = {'folderName': 'greenteksolutions'}
         files = [
@@ -49,14 +44,9 @@ def format_testing_report():
             'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'))
         ]
         headers = {}
-
         response = requests.request("POST", URL, headers=headers, data=payload, files=files)
 
-        print(download_url)
-        print(response.text)
-        print(response.status_code)
-
-        # return download url
+        # Return download url
         return jsonify({
             'download_url':download_url,
             'upload_status': response.status_code,
