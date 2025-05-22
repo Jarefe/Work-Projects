ae900fd35e2c98e95d3652d161d1dfd9cc772c4c
Jared <jarefe2000@gmail.com>
2025-05-22 14:58:10 -0500

    WIP auto attributing from extreme attribute excel file

---
+++ ExcelFormatAPI/Auto_Attribute.py
@@ -0,0 +1,184 @@
+import os
+
+import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DASH_INVENTORY_HEADERS = [
+    'Condition',
+    'Item',
+    'Brand',
+    'Description',
+    'SN',
+    'Category',
+    'Inv-ID',
+    'PO-Line',
+    'Status'
+]
+
+DESKTOP_HEADERS = [
+    "PO#",
+    "Line#",
+    "Condition",
+    "MFGR",
+    "Item#",
+    "Description",
+    "IQ Inv-ID",
+    "SN",
+    "Status",
+    "Processor",
+    "Type of RAM",
+    "RAM Capacity",
+    "Drive Caddy",
+    "Drive Capacity",
+    "Drive Interface",
+    "GPU",
+    "Wi-Fi",
+    "Form Factor",
+    "Notes",
+    "Location",
+    "Cost",
+    "Vendor"
+]
+
+LAPTOP_HEADERS = [ # = dealt with
+    "PO#",#
+    "Line#",#
+    "Condition",#
+    "MFGR",#
+    "Item#",#
+    "Description",#
+    "IQ Inv-ID",#
+    "SN",#
+    "Status",#
+    "Processor",#
+    "Type of RAM",#
+    "RAM Capacity",#
+    "Battery",#
+    "Touchscreen", # does not show in eps
+    "GPU",#
+    "Drive Caddy",# does not show in eps
+    "Drive Capacity",#
+    "Drive Interface",#
+    "Notes",# does not show in eps
+    "Location",#
+    "Cost",#
+    "Vendor"# does not show in eps
+]
+
+EXTREME_LAPTOP_HEADERS = [
+    "Item", "Brand", "SN", "Category", "Description", "Condition", "PO-Line", "QTY", "Cost", "Status",
+    "Receive Status", "SO-Line", "SO Rep", "Warehouse", "PO Rep", "Update Date", "Customer Asset Tag",
+    "Location", "Container ID", "Virtual Container", "Testing Status", "Added", "Date Received", "ID",
+    "IQ Inventory ID", "System Manufacturer", "System Model", "System Serial Number",
+    "System Serial Number New", "System SKU", "System Processor Information", "System Memory Information",
+    "HDD Manufacturer #1", "HDD Model #1", "HDD Serial Number #1", "HDD Capacity #1", "HDD Block Size #1",
+    "HDD RPM #1", "HDD Form Factor #1", "HDD Device Type #1", "HDD Health Score #1", "HDD Grade #1",
+    "HDD Reallocated Sectors #1", "HDD Power On Hours #1", "HDD Erasure Method #1",
+    "HDD Erasure Results #1", "HDD Erasure Start Date #1", "HDD Erasure End Date #1",
+    "HDD Erasure Start Time #1", "HDD Erasure End Time #1", "CPU Manufacturer #1", "CPU Model #1",
+    "CPU Serial Number #1", "Processor Speed #1", "Processor Cores #1", "Processor Type #1",
+    "Memory Manufacturer #1", "Memory Model #1", "Memory Serial Number #1", "Memory Size #1",
+    "Memory Speed #1", "Memory Type #1", "Testing Serial Number #1", "Testing Profile #1",
+    "Testing CPU #1", "Testing Memory #1", "Testing Battery #1", "Testing Missing Parts #1",
+    "Testing Case Condition #1", "Testing Screen Condition #1", "Testing Display Resolution #1",
+    "Testing Optical Device #1", "Battery Manufacturer #1", "Battery Model #1", "Battery Serial Number #1",
+    "Battery Chemistry #1", "Battery Capacity #1", "Battery Full Charge #1", "Battery Life #1",
+    "Battery Drain Test Results #1", "Component Manufacturer #1", "Component Model #1",
+    "Component Serial Number #1", "Component Manufacturer #2", "Component Model #2",
+    "Component Serial Number #2", "Component Manufacturer #3", "Component Model #3",
+    "Component Serial Number #3", "Component Manufacturer #4", "Component Model #4",
+    "Component Serial Number #4", "Component Manufacturer #5", "Component Model #5",
+    "Component Serial Number #5", "Component Manufacturer #6", "Component Model #6",
+    "Component Serial Number #6", "Network Adapter #1", "Video Adapter #1"
+]
+
+EXTREME_DESKTOP_HEADERS = [
+    "Item", "Brand", "SN", "Category", "Description", "Condition", "PO-Line", "QTY", "Cost", "Status",
+    "Receive Status", "SO-Line", "SO Rep", "Warehouse", "PO Rep", "Update Date", "Customer Asset Tag",
+    "Location", "Container ID", "Virtual Container", "Testing Status", "Added", "Date Received", "ID",
+    "IQ Inventory ID", "System Manufacturer", "System Model", "System Serial Number",
+    "System Serial Number New", "System SKU", "System Processor Information", "System Memory Information",
+    "HDD Manufacturer #1", "HDD Model #1", "HDD Serial Number #1", "HDD Capacity #1", "HDD Block Size #1",
+    "HDD RPM #1", "HDD Form Factor #1", "HDD Device Type #1", "HDD Health Score #1", "HDD Grade #1",
+    "HDD Reallocated Sectors #1", "HDD Power On Hours #1", "HDD Erasure Method #1",
+    "HDD Erasure Results #1", "HDD Erasure Start Date #1", "HDD Erasure End Date #1",
+    "HDD Erasure Start Time #1", "HDD Erasure End Time #1", "CPU Manufacturer #1", "CPU Model #1",
+    "CPU Serial Number #1", "Processor Speed #1", "Processor Cores #1", "Processor Type #1",
+    "Memory Manufacturer #1", "Memory Model #1", "Memory Serial Number #1", "Memory Size #1",
+    "Memory Speed #1", "Memory Type #1", "Testing Serial Number #1", "Testing Profile #1",
+    "Testing CPU #1", "Testing Memory #1", "Testing Battery #1", "Testing Missing Parts #1",
+    "Testing Case Condition #1", "Testing Screen Condition #1", "Testing Display Resolution #1",
+    "Testing Optical Device #1", "Component Manufacturer #1", "Component Model #1",
+    "Component Serial Number #1", "Component Manufacturer #2", "Component Model #2",
+    "Component Serial Number #2", "Component Manufacturer #3", "Component Model #3",
+    "Component Serial Number #3", "Component Manufacturer #4", "Component Model #4",
+    "Component Serial Number #4", "Component Manufacturer #5", "Component Model #5",
+    "Component Serial Number #5", "Network Adapter #1", "Video Adapter #1"
+]
+
+def process_dash(df: pd.DataFrame):
+    temp = df.copy()
+    temp.rename(columns={'IQ Inventory ID' : 'Inv-ID'}, inplace=True)
+    dash_df = temp[DASH_INVENTORY_HEADERS]
+
+    return dash_df
+
+def process_extreme_laptops(df: pd.DataFrame):
+    dash_df = process_dash(df)
+    # Rename columns to match
+    rename = {
+        'Item' : 'Item#',
+        'System Manufacturer' : 'MFGR',
+        'IQ Inventory ID' : 'IQ Inv-ID',
+        'System Serial Number' : 'SN',
+        'System Processor Information' : 'Processor',
+        'Memory Type #1' : 'Type of RAM',
+        'Memory Size #1' : 'RAM Capacity',
+        'Testing Battery #1' : 'Battery',
+        'Video Adapter #1' : 'GPU',
+        'HDD Capacity #1' : 'Drive Capacity',
+        'HDD Form Factor #1' : 'Drive Interface'
+    }
+    df.rename(columns=rename, inplace=True)
+
+    laptop_df = pd.DataFrame(index=df.index, columns=LAPTOP_HEADERS)
+
+    # FIXME: copying data to new dataframe
+
+
+    # split PO line to 2 columns
+    po_line_df = df['PO-Line'].str.split('-', expand=True)
+
+    laptop_df['PO#'] = po_line_df['PO#'].values
+    laptop_df['Line#'] = po_line_df['Line#'].values
+
+
+    return dash_df, laptop_df
+
+def process_extreme_desktops(df: pd.DataFrame):
+    pass
+
+def process_extreme_attributes(filepath: str):
+    try:
+        raw_dataframe = pd.read_excel(filepath)
+
+        category = raw_dataframe['Category'].iloc[0]
+
+        if category == 'PC':
+            return process_extreme_desktops(raw_dataframe)
+        elif category == 'Laptop':
+            return process_extreme_laptops(raw_dataframe)
+
+        return None
+
+    except Exception as ex:
+        raise ex
+
+if __name__ == '__main__':
+    try:
+        print(process_extreme_attributes(os.getenv('AUTO_ATT_TESTING')))
+
+    except Exception as e:
+        print(f'error occurred: {e}')
